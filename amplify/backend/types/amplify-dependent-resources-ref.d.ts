b551bfe6cf9921a0406110ae3af263c1479cbbd7
z-cui-233 <z-cui@unext.jp>
2022-03-07 16:56:49 +0900

    Add TODO feature and connect it to backend

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "amplifytestapp": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { DataStore } from "@aws-amplify/datastore";
+import { Todo } from "./models";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [todos, setTodos] = useState(null);
+
+  async function queryTodos() {
+    const models = await DataStore.query(Todo);
+    setTodos(models);
+    console.log(models);
+  }
+
+  useEffect(() => {
+    queryTodos();
+  }, []);
+
+  const [newTodo, setNewTodo] = useState("");
+
+  const handleAddTodo = async () => {
+    await DataStore.save(
+      new Todo({
+        name: newTodo,
+        description: "No description",
+      })
+    );
+    await queryTodos();
+    setNewTodo("");
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +46,34 @@ function App() {
           Learn React Test
         </a>
       </header>
+      <div style={{ height: "100vh" }}>
+        <h3>TODO list</h3>
+        <input
+          type="text"
+          value={newTodo}
+          onChange={(event) => setNewTodo(event.target.value)}
+        ></input>
+        <button onClick={handleAddTodo}>Add</button>
+        {todos?.map((todo) => (
+          <label key={todo.text} style={{ display: "block" }}>
+            <input
+              type="checkbox"
+              checked={todo.checked}
+              onChange={() => {}}
+            ></input>
+            {todo.name}
+            <button
+              onClick={async () => {
+                const modelToDelete = await DataStore.query(Todo, todo.id);
+                DataStore.delete(modelToDelete);
+                queryTodos();
+              }}
+            >
+              Remove
+            </button>
+          </label>
+        ))}
+      </div>
     </div>
   );
 }
